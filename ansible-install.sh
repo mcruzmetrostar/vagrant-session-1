b29bbfb7e96c9c0a9bde2e72f7467a0a22116edb
michaelcalizo <michael.calizo@datacom.co.nz>
2017-03-27 13:39:27 +1300

    updated ansible-install.sh to put a bash condition if package is installed

--- ansible-install.sh
+++ ansible-install.sh
@@ -2,10 +2,16 @@ rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch
 sudo yum repolist
 sudo yum install ansible -y 
 sudo yum install telnet -y
+sudo yum install git -y
 mkdir /home/vagrant/playbooks
 chown vagrant:vagrant /home/vagrant/playbooks
 mkdir /home/vagrant/playbooks/keys
 chown vagrant:vagrant /home/vagrant/playbooks/keys
 chmod 755 /home/vagrant/playbooks/keys
+chmod 755 /home/vagrant/playbooks/keys/vagrant
 sudo yum install lsb-core-noarch -y
-#rpm -Uvh https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm
+rpm -qa | grep atom
+if [ $? -eq 1 ]; then 
+#  echo "This is the previous output $?	"
+  rpm -ivh https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm
+fi
